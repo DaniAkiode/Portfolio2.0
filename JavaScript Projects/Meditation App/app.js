f3a1f85072052f73a96c960559b64cc5fb36dfde
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-10-23 14:08:40 +0100

    Can now select sounds and time

--- JavaScript Projects/Meditation App/app.js
+++ JavaScript Projects/Meditation App/app.js
@@ -8,7 +8,9 @@ const app = () => {
     const sounds = document.querySelectorAll('.sound-picker button');
     // Time display
     const timeDisplay = document.querySelector('.time-display');
-     //get the length of the outline
+    const timeSelect = document. querySelectorAll('.time-select button');
+    
+    //get the length of the outline
     const outlineLength = outline.getTotalLength(); 
     //duration
     let fakeDuration = 600;
@@ -16,10 +18,33 @@ const app = () => {
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength
 
+    //play different sounds
+    
+    sounds.forEach(sound =>{
+        sound.addEventListener('click',function(){
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
+
+    });
+
+
     //play sound
     play.addEventListener('click', () => {
         checkPlaying(song);
     });
+
+
+    //Select sounds
+    timeSelect.forEach(option => {
+       option.addEventListener('click', function(){
+           fakeDuration = this.getAttribute('data-time');
+           timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}: ${Math.floor(
+               fakeDuration % 60
+               )}`;
+       }) 
+    })
 
 
     //create a function specific to stop and play the sounds  
@@ -33,10 +58,32 @@ const app = () => {
             video.pause();
             play.src = './svg/play.svg'; 
         }
-    } 
+    };
+    
+    // animate the circle 
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60); 
 
+        //Animate bar
 
-}
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        //Animate text
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+    
+        if(currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        }
+    
+    };
+};
 
 
 app();
