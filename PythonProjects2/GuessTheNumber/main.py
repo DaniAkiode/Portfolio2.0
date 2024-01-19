2668cf22487e7cacbbbba0c7fb7fc935061dda9b
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-01-19 11:30:45 +0000

    The Computer is guessing

--- PythonProjects2/GuessTheNumber/main.py
+++ PythonProjects2/GuessTheNumber/main.py
@@ -11,7 +11,26 @@ def guess(x):
             print("Sorry, guess again. Too High.")
     
     print(f"YESS BOSS, YOU GUEST DE NOMBA  {random_number} correctly")
+
+def computer_guess(x):
+    low = 1
+    high = x 
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could be high b/c low = high
+        
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)!!').lower()
+        if feedback == 'h':
+            high = guess -1 
+        elif feedback == 'l':
+            low = guess + 1 
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
             
 
-guess(10)
+computer_guess(1000)
+#guess(10)
 
