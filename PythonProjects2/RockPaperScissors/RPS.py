bce28e49fdc0db07b4c1880d2952c57bd9284ea5
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-01-19 12:36:47 +0000

    RPS Complete

--- PythonProjects2/RockPaperScissors/RPS.py
+++ PythonProjects2/RockPaperScissors/RPS.py
@@ -1,17 +1,25 @@
 import random
 
 def play():
-    user = input("r = rock, p = paper, s = scissors")
-    computer = random.choice['r', 'p','s']
+    user = input("What's your choice? r = rock, p = paper, s = scissors\n")
+    computer = random.choice(['r', 'p','s'])
 
     if user == computer:
-        return 'tie'
+        return 'It\'s a tie'
     
     # r > s , s > p, p > r  
+    if is_win(user, computer):
+        return 'You won!'
+    
+    return 'You Lost!'
 
 def is_win(player, opponent):
     # return true if player wins 
     # r > s , s > p, p > r  ]
-    '''if (player == 'r' and opponent == 's') or (player = 's' and oppenent == 'p') \
-    or (player == '')'''
+    if (player == 'r' and opponent == 's') \
+        or (player == 's' and opponent == 'p') \
+        or (player =="p" and opponent == 'r'):
+        return True  
+            
 
+print(play())
